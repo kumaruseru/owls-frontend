aff8ee83a4fd1fbe060740ab76e56cfaebc2e3e8
kumaruseru <nghiaht281003@gmail.com>
2025-12-28 11:07:43 +0700

    fix deps for react 19

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactLenis } from '@studio-freight/react-lenis'
+import { ReactLenis } from 'lenis/react'
 import { Toaster } from 'react-hot-toast';
 import { MaintenanceCheck } from '@/components/common/MaintenanceCheck';
 
